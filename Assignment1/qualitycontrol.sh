083494bcbe4a13618fcd2815ff54194829040c37
Andreas Fellas <s1207673@bioinfmsc5.bio.ed.ac.uk>
2019-10-12 19:32:53 +0100

    fastqc integration

--- Assignment1/qualitycontrol.sh
+++ Assignment1/qualitycontrol.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATA_DIR=$SCRIPT_DIR/fastq
+RES_DIR=$SCRIPT_DIR/results
+
+if [ -d "$RES_DIR" ]; then rm -Rf $RES_DIR; fi
+
+mkdir $RES_DIR
 
 while IFS= read -r line
 do
@@ -8,14 +14,23 @@ do
 
   experiment_code=${args[0]}
   experiment_type=${args[1]}
-  pair_file_1=${args[2]}
-  pair_file_2=${args[3]}
+  pair_file_1=$DATA_DIR/${args[2]}
+  pair_file_2=$DATA_DIR/${args[3]}
+  #echo $experiment_code
+  #echo $experiment_type
+  #echo $pair_file_1
+  #echo $pair_file_2
 
-  echo $experiment_code
-  echo $experiment_type
-  echo $pair_file_1
-  echo $pair_file_2
-done < $SCRIPT_DIR/fastq/fqfiles
+  experiment_dir=$RES_DIR/$experiment_code
+  mkdir $experiment_dir
+
+  echo "Doing experiment $experiment_code"
+  fastqc $pair_file_1 -o $experiment_dir
+  fastqc $pair_file_2 -o $experiment_dir
+  echo "Done!"
+  
+
+done < $DATA_DIR/fqfiles
 
 # Quality check
 #fastqc /localdisk/data/BPSM/Assingment1/fastq/*fq.gz -o
