bc587e1332a6de539f105d1c8e23c13201a5f777
Andreas Fellas <s1207673@bioinfmsc5.bio.ed.ac.uk>
2019-10-12 18:59:29 +0100

    itteration variables created

--- Assignment1/qualitycontrol.sh
+++ Assignment1/qualitycontrol.sh
@@ -2,7 +2,20 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo $SCRIPT_DIR
+while IFS= read -r line
+do
+  args=($line)
+
+  experiment_code=${args[0]}
+  experiment_type=${args[1]}
+  pair_file_1=${args[2]}
+  pair_file_2=${args[3]}
+
+  echo $experiment_code
+  echo $experiment_type
+  echo $pair_file_1
+  echo $pair_file_2
+done < $SCRIPT_DIR/fastq/fqfiles
 
 # Quality check
 #fastqc /localdisk/data/BPSM/Assingment1/fastq/*fq.gz -o
